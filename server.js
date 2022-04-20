ba31bd9ef1ba2c343b4d418bd4115ac21fe8f003
SamanthaPace <62861475+SamanthaPace@users.noreply.github.com>
2022-04-19 21:10:13 -0400

    Updated servers HTTPs

--- server.js
+++ server.js
@@ -15,34 +15,34 @@ app.use(cors({ credentials: true, origin: ['http://localhost:3000'] }));
 app.get('/main', routes.generic_movie_search)
 
 // Route 2 - register as GET 
-app.get('/movies', routes.top_movie_by_year)
+app.get('/moviesByYear', routes.top_movie_by_year)
 
 // Route 3 - register as GET 
-app.get('/movies', routes.top_movie_by_genre)
+app.get('/moviesByGenre', routes.top_movie_by_genre)
 
 // Route 4 - register as GET 
-app.get('/movies', routes.top_movie_by_director)
+app.get('/moviesByDirector', routes.top_movie_by_director)
 
 // Route 5 - register as GET 
-app.get('/movies', routes.movie_id)
+app.get('/movieID', routes.movie_id)
 
 // Route 6 - register as GET 
-app.get('/movies', routes.new_top_rated)
+app.get('/newTopRated', routes.new_top_rated)
 
 // Route 7 - register as GET 
-app.get('/movies', routes.top_rated_by_demographics)
+app.get('/topDemographic', routes.top_rated_by_demographics)
 
 // Route 8 - register as POST 
-app.post('/movies', routes.add_to_watchlist)
+app.post('/addMovieToWatchlist', routes.add_to_watchlist)
 
 // Route 9 - register as GET 
 app.get('/watchlist', routes.watchlist)
 
 // Route 10 - register as GET 
-app.get('/watchlist', routes.recommendation)
+app.get('/recommendation', routes.recommendation)
 
 // Route 11 - register as GET 
-app.get('/main', routes.all_movies)
+app.get('/allMovies', routes.all_movies)
 
 app.listen(config.server_port, () => {
     console.log(`Server running at http://${config.server_host}:${config.server_port}/`);
