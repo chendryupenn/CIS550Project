a044f3a75d8c8d74e3a30aefa7a99f6fd8afabec
chendryupenn <81385472+chendryupenn@users.noreply.github.com>
2022-04-26 17:24:04 -0400

    Update MovieList.js

--- MovieList.js
+++ MovieList.js
@@ -6,7 +6,12 @@ const MovieList = (props) => {
 			{props.movies.map((movie, index) => (
 				<div className='image-container d-flex justify-content-start m-3'>
 					<img src={movie.poster} alt='movie'></img>
-		
+					<div
+						onClick={() => props.goToMovie(movie.imdb_id)}
+						className='overlay d-flex align-items-center justify-content-center'
+					>
+
+					</div>
 				</div>
 			))}
 		</>
