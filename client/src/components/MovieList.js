43594beb2114620fc8b924cf34fbc081f60db184
chendryupenn <81385472+chendryupenn@users.noreply.github.com>
2022-04-27 18:54:56 -0400

    Update MovieList.js

--- client/src/components/MovieList.js
+++ client/src/components/MovieList.js
@@ -1,12 +1,17 @@
 import React from 'react';
 
+	
 const MovieList = (props) => {
+	function goToMovie(MovieId){
+		window.location = `/movies?id=${MovieId}`
+	};
+
 	return (
 		<>
 			{props.movies.map((movie, index) => (
 				<div className='image-container d-flex justify-content-start m-3'>
-					<img src={movie.poster} alt='movie'></img>
-		
+					<img src={movie.poster} onClick={() => goToMovie(movie.imdb_id)} />
+						
 				</div>
 			))}
 		</>
